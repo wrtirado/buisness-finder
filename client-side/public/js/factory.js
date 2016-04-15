88f7cc0377610ebbd8a7804e37958ab01dc481a7
wrtirado <wrtirado@gmail.com>
2016-04-15 16:09:55 -0600

    Possibly finished the factory.js page. working now on the front end controller

--- client-side/public/js/factory.js
+++ client-side/public/js/factory.js
@@ -9,36 +9,36 @@
     var businessData = {},
         apiUrl = 'http://morning-gorge-14837.herokuapp.com/api/v1/businesses'
 
-    restaurantData.getAll = function () {
+    businessData.getAll = function () {
       console.log('getting all businesses')
       return $http.get(apiUrl)
     }
 
-    restaurantData.create = function (restaurant) {
-      console.log('2 - client: Calling the http verb \'post\', and as a result is crossing the divide over to the server side. More specifically, it is looking at where I defined the api_routes, and where this specific route is pointing within the restaurants_controller!')
-      var result = $http.post(apiUrl, restaurant)
+    businessData.create = function (business) {
+      console.log('2 - client: Calling the http verb \'post\', and as a result is crossing the divide over to the server side. More specifically, it is looking at where I defined the api_routes, and where this specific route is pointing within the mapigatorController!')
+      var result = $http.post(apiUrl, business)
       console.log('5 - client: running inside the factory.js file')
       return result
     }
 
-    restaurantData.getSingle = function(id){
-      console.log('getting single restaurant data:',id)
+    businessData.getSingle = function(id){
+      console.log('getting single business data:',id)
       return $http.get(apiUrl + '/' + id)
     }
 
-    restaurantData.destroy = function(id){
-      console.log('2 - client: Calling the http verb \'delete\', and as a result is crossing the divide over to the server side. More specifically, it is looking at where I defined the api_routes, and where this specific route is pointing within the restaurants_controller!')
+    businessData.destroy = function(id){
+      console.log('2 - client: Calling the http verb \'delete\', and as a result is crossing the divide over to the server side. More specifically, it is looking at where I defined the api_routes, and where this specific route is pointing within the mapigatorController!')
       var result = $http.delete(apiUrl + '/' + id)
       console.log('5 - client: running inside the factory.js file')
       return result
     }
 
-    restaurantData.update = function(id, restaurant){
-      console.log('2 - client: Calling the http verb \'put\', and as a result is crossing the divide over to the server side. More specifically, it is looking at where I defined the api_routes, and where this specific route is pointing within the restaurants_controller!')
-      var result = $http.put(apiUrl + '/' + id, restaurant)
-      console.log('6 - recieving the response from the res.send in the restaurants_controller');
+    businessData.update = function(id, business){
+      console.log('2 - client: Calling the http verb \'put\', and as a result is crossing the divide over to the server side. More specifically, it is looking at where I defined the api_routes, and where this specific route is pointing within the mapigatorController!')
+      var result = $http.put(apiUrl + '/' + id, business)
+      console.log('6 - recieving the response from the res.send in the mapigatorController');
       return result
     }
-    return restaurantData
+    return businessData
   }
 }())
